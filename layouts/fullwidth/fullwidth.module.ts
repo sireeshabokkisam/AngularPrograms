3eab6ffd17cd0f8d4fa07953be2a0e88aaf173f8
sireeshabokkisam <61773226+sireeshabokkisam@users.noreply.github.com>
2020-05-10 22:42:09 +0530

    initial commit

---
+++ layouts/fullwidth/fullwidth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullwidthComponent } from './fullwidth.component';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    FullwidthComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    SharedModule
+  ]
+})
+export class FullwidthModule { }
